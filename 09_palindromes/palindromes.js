6b9a53451148e6ca77959c3e343b5e21fbdcc3d7
HL <han.pecktree@gmail.com>
2023-02-26 15:04:01 -0700

    Complete 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    string = string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()" "]/g,"")
+    .toLowerCase();
 
+    if (string === string.split("").reverse().join("")) return true;
+    return false;
+    
 };
 
 // Do not edit below this line
