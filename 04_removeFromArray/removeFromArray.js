e632611e87884be419128f2a77a8c27b118727ac
HL <han.pecktree@gmail.com>
2023-02-26 13:57:58 -0700

    Complete up to 06

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...toRemove) {
+    for (i = 0; i < toRemove.length; i++) {
+        if (toRemove[i] !== arr[arr.indexOf(toRemove[i])] || !arr.includes(toRemove[i])) continue;
+        arr.splice(arr.indexOf(toRemove[i]), 1);
+    }
+    return arr;
 };
 
 // Do not edit below this line
