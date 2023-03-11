3cd9fdf4e891a346997a88605c091dd27316f96a
HL <han.pecktree@gmail.com>
2023-03-10 20:54:04 -0700

    Complete up to 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const oldest = people.sort((a,b) => {
+        if(!a.yearOfDeath) a.yearOfDeath = new Date().getFullYear();
+        if(!b.yearOfDeath) b.yearOfDeath = new Date().getFullYear();
+        const personA = a.yearOfDeath - a.yearOfBirth;
+        const personB = b.yearOfDeath - b.yearOfBirth;
+        return personA > personB ? -1 : 1;
+    });
+    return oldest[0];
 };
 
 // Do not edit below this line
