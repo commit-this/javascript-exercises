4cec6d6c21246dc21f412f289dd1748f063bb666
HL <han.pecktree@gmail.com>
2023-04-09 15:38:39 -0600

    Complete 13

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,31 @@
-const caesar = function() {
+const caesar = function(str, key) {
+    let shiftedStr = '';
+    // if key is negative, convert to positive
+    if (key < 0) {
+        key = 26 + (key % 26);
+    }
 
+    for (i = 0; i < str.length; i++) {
+        // check for non-alphabetic character
+        if (str[i].toLowerCase() === str[i].toUpperCase()) {
+            shiftedStr += str[i];
+            continue;
+        }
+
+        // get unicode number for character
+        code = str.charCodeAt(i);
+        let shiftedCode = 0
+
+        // caesar algorithm
+        if (str[i] === str[i].toUpperCase()) {
+            shiftedCode = ((code - 65 + key) % 26) + 65;
+        } else {
+            shiftedCode = ((code - 97 + key) % 26) + 97;
+        }
+        // convert back to string
+        shiftedStr += String.fromCharCode(shiftedCode);
+    }
+    return shiftedStr;
 };
 
 // Do not edit below this line
